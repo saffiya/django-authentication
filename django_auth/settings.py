fe1c570772607491c7db493ec0bafe94790e927d
Ubuntu <ubuntu@ip-172-31-33-23.ec2.internal>
2020-05-19 11:03:01 +0000

    Added custom authentication

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -100,6 +100,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -122,10 +127,10 @@ STATIC_URL = '/static/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.Emailbackend"
+#EMAIL_BACKEND = "django.core.mail.backends.console.Emailbackend"
 
-#EMAIL_USE_TLS = True
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-#EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-#EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
